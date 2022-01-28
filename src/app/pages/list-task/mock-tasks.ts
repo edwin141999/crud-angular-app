fa3282d978ba35eac7cac836fd1ea8238a4a2bcc
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-27 21:21:29 -0600

    testing data

---
+++ src/app/pages/list-task/mock-tasks.ts
@@ -0,0 +1,10 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+  { id: 1, title: 'tarea1', description: 'descripcion1', done: false },
+  { id: 2, title: 'tarea2', description: 'descripcion2', done: false },
+  { id: 3, title: 'tarea3', description: 'descripcion3', done: false },
+  { id: 4, title: 'tarea4', description: 'descripcion4', done: false },
+  { id: 5, title: 'tarea5', description: 'descripcion5', done: false },
+  { id: 6, title: 'tarea6', description: 'descripcion6', done: false },
+];
