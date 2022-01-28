92be73a5854ab199a2c304f81eed135c5b88157a
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-27 21:22:19 -0600

    update ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UpdateTaskComponent } from './pages/update-task/update-task.component';
+import { ListTaskComponent } from './pages/list-task/list-task.component';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    UpdateTaskComponent,
+    ListTaskComponent,
+    NavbarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
