23ad6994d2f00fedabec5315f9364cc1c9408cc8
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-02-01 00:23:14 -0600

    component add-task update

--- src/app/pages/add-task/add-task.component.ts
+++ src/app/pages/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Task } from 'src/app/models/task';
+import { FirestoreService } from 'src/app/services/firestore/firestore.service';
 
 @Component({
   selector: 'app-add-task',
@@ -6,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+  constructor(private _db: FirestoreService, private _fb: FormBuilder) {
+    this.form = _fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      done: [false],
+    });
+  }
 
   ngOnInit(): void {}
+
+  addTask() {
+    const TASK: Task = {
+      title: this.form.value.title,
+      description: this.form.value.description,
+      done: false,
+    };
+    this._db.add_Task(TASK).then(
+      () => {
+        console.log(TASK);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
